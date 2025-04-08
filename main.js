e3c2886075caa00f87c5dd1baeb738aaff59e734
LucasTsuzuku-SPTECH <lucas.tsuzuku@sptech.school>
2025-04-08 10:41:38 -0300

    alteracoes parte 1 arq comp - testes

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 // função para comunicação serial
 const serial = async (valoresSensorAnalogico, valoresSensorDigital) => {
   // conexão com o banco de dados MySQL
   let poolBancoDados = mysql
     .createPool({
-      host: "HOST_DO_BANCO",
-      user: "USUARIO_DO_BANCO",
-      password: "SENHA_DO_BANCO",
-      database: "DATABASE_DO_BANCO",
+      host: "127.0.0.1",
+      user: "dryflow",
+      password: "123456",
+      database: "dryflow01",
       port: 3306,
     })
     .promise();
@@ -67,7 +67,7 @@ const serial = async (valoresSensorAnalogico, valoresSensorDigital) => {
       if (HABILITAR_OPERACAO_INSERIR) {
         // este insert irá inserir os dados na tabela "medida"
         await poolBancoDados.execute(
-          "INSERT INTO medida (sensor_analogico, sensor_digital) VALUES (?, ?)",
+          "INSERT INTO valores (valorAnalogico, valorDigital) VALUES (?, ?)",
           [sensorAnalogico, sensorDigital]
         );
         console.log(
